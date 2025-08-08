e141b9019bf66ccf3e8d24376a6976e4180c4d25
customercaremolvietnam <customercare.molvietnam@gmail.com>
2025-08-08 13:27:14 +0700

    Add files via upload

---
+++ sources.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { name: 'VnExpress', url: 'https://vnexpress.net/rss/tin-moi-nhat.rss' },
+  { name: 'Tuổi Trẻ', url: 'https://tuoitre.vn/rss/tin-moi-nhat.rss' },
+  { name: 'Thanh Niên', url: 'https://thanhnien.vn/rss/home.rss' },
+  { name: 'Vietnamnet', url: 'https://vietnamnet.vn/rss/tin-moi-nhat.rss' },
+  { name: 'Dân Trí', url: 'https://dantri.com.vn/rss/tin-moi-nhat.rss' },
+  { name: 'Cafef', url: 'https://cafef.vn/rss/tin-moi-nhat.rss' },
+  { name: 'VNReview', url: 'https://vnreview.vn/rss/tin-moi.rss' },
+  { name: 'ICTNews', url: 'https://ictnews.vietnamnet.vn/rss/tin-moi-nhat.rss' },
+  { name: 'GenK', url: 'https://genk.vn/rss/home.rss' },
+  { name: 'Người Lao Động', url: 'https://nld.com.vn/rss/home.rss' }
+];
